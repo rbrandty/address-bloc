ca7ce37b1363c1e03b061d60ffba8fb6b7e4eb15
Rachel Brandt <rachel.brandt@gmail.com>
2017-07-07 15:04:27 -0400

    assignment4

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -17,22 +17,39 @@ RSpec.describe AddressBook do
       expect(book.entries.size).to eq(0)
     end
   end
-  describe "#add_entry" do
-       it "adds only one entry to the address book" do
-         book = AddressBook.new
-         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
-         expect(book.entries.size).to eq(1)
-       end
+  describe "#remove_entry" do
+    it "removes an entry using the name, phone_number and email"
+    book = AddressBook.new
+    book.add_entry('Michael Faraday', '010.210.5181', 'michael.faraday@faraday.com')
 
-       it "adds the correct information to entries" do
-         book = AddressBook.new
-         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-         new_entry = book.entries[0]
+    name = "Ada Lovelace"
+    phone_number = "101.102.1815"
+    email_address = "augusta.king@lovelace.com"
+    book.add.entry(name, phone_number, email)
 
-         expect(new_entry.name).to eq('Ada Lovelace')
-         expect(new_entry.phone_number).to eq('010.012.1815')
-         expect(new_entry.email).to eq('augusta.king@lovelace.com')
-       end
-     end
-   end
+    expect(book.entries.size).to eq 2
+    book.remove_entry(name, phone_number, email)
+    expect(book.entries.size).to eq 1
+    expect(book.entries.first.name).to eq("Michael Faraday")
+  end
+end
+
+describe "#add_entry" do
+  it "adds only one entry to the address book" do
+    book = AddressBook.new
+    book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+    expect(book.entries.size).to eq(1)
+  end
+
+  it "adds the correct information to entries" do
+    book = AddressBook.new
+    book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    new_entry = book.entries[0]
+
+    expect(new_entry.name).to eq('Ada Lovelace')
+    expect(new_entry.phone_number).to eq('010.012.1815')
+    expect(new_entry.email).to eq('augusta.king@lovelace.com')
+  end
+end
