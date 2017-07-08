8e93641f3d9879e14465fcbdf35f7c6190163b19
Rachel Brandt <rachel.brandt@gmail.com>
2017-07-08 06:28:35 -0400

    adding controllers checkpoint5

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,7 +9,7 @@ class AddressBook
   def remove_entry(name, phone_number, email)
     delete_entry = nil
 
-    @entries.each do |entry
+    @entries.each do |entry|
       if name == entry.name && phone == entry.phone_nmber && email == entry.email
         delete_entry = entry
       end
